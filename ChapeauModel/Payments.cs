a5190dd2650bdb74c08e85cbe40596a31b796e83
manolinholland02 <678785@student.inholland.nl>
2022-05-19 19:49:46 +0200

    ad

---
+++ ChapeauModel/Payments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChapeauModel
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public DateTime DateOfPayment { get; set; }
+        public decimal PaymentPrice { get; set; }
+        public decimal Tip { get; set; }
+        public int TableId { get; set; }
+    }
+}
