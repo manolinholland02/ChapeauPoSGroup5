97545d5b9ddf0ba03032a700ad3d97b1ad321353
manolinholland02 <678785@student.inholland.nl>
2022-05-20 11:33:59 +0200

    a

--- ChapeauModel/DinnerMenu.cs
+++ ChapeauModel/DinnerMenu.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChapeauModel
 {
-    class DinnerMenu
+    public class DinnerMenu
     {
     }
 }
--- ChapeauModel/LunchMenu.cs
+++ ChapeauModel/LunchMenu.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChapeauModel
 {
-    class LunchMenu
+    public class LunchMenu
     {
     }
 }
