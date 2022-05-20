488baae4bfccab7b78609e9e8657163981d66c21
manolinholland02 <678785@student.inholland.nl>
2022-05-20 14:23:53 +0200

    da

--- ChapeauModel/Orders.cs
+++ ChapeauModel/Orders.cs
@@ -12,19 +12,13 @@ namespace ChapeauModel
         public int orderTable { get; set; }
         public MenuItem orderItem { get; set; }
         public string orderItemName { get; set; }
-
         public int ItemQuantity { get; set; }
         public string orderComment { get; set; }
-
         public decimal orderPrice { get; set; }
-
         public Status orderStatus { get; set; }
-
         public Employee orderWaiter { get; set; }
         public Employee orderPreparer { get; set; }
-
         public Payment orderPayment { get; set; }
-
     }
 
 }
