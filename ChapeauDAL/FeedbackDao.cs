b9111bf702e61d9639a8c0f6395eb551585ad6c9
Sali2410 <678338@student.inholland.nl>
2022-05-20 13:57:44 +0200

    feedback

--- ChapeauDAL/FeedbackDao.cs
+++ ChapeauDAL/FeedbackDao.cs
@@ -11,11 +11,34 @@ namespace ChapeauDAL
 {
     class FeedbackDao : BaseDao
     {
-        public List<Feedback> AddFeedback()
+        
+        public List<Feedback> GetAllFeedback()
         {
             string query = "SELECT [tableId], [feedbackId], [dateOfFeedback], [typeOfFeedback], [note] FROM [Feedbacks]";
             SqlParameter[] sqlParameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+        
+
+        public void AddFeedback(Feedback feedback)
+        {
+            try
+            {
+                SqlParameter[] parameters =
+                {
+                    new SqlParameter("@tableId", feedback.TableId),
+                    new SqlParameter("@feedbackId", feedback.FeedbackId),
+                    new SqlParameter("@dateOfFeedback", feedback.DateOfFeedback),
+                    new SqlParameter("@typeOfFeedback", feedback.TypeOfFeedback),
+                    new SqlParameter("@note", feedback.Note)
+                };
+                string query = $"INSERT INTO [dbo.Feedbacks] VALUES (@note, @typeOfFeedback) WHERE (tableId = @tableId AND feedbackId = @feedbackId)";
+                ExecuteEditQuery(query, parameters);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
         public List<Feedback> ReadTables(DataTable dataTable)
         {
@@ -34,5 +57,6 @@ namespace ChapeauDAL
             }
             return feedback;
         }
+
     }
 }
--- ChapeauLogic/FeedbackService.cs
+++ ChapeauLogic/FeedbackService.cs
@@ -10,5 +10,9 @@ namespace ChapeauLogic
 {
     class FeedbackService
     {
+
+        
+
+
     }
 }
