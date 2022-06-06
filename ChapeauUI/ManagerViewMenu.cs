9ec042990de63cb7f157f8ea6e2c8cc59d50de11
manolinholland02 <678785@student.inholland.nl>
2022-06-06 19:11:49 +0300

    trudno se jivee sred glupci nerazbrani

--- ChapeauUI/ManagerViewMenu.cs
+++ ChapeauUI/ManagerViewMenu.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChapeauLogic;
+using ChapeauModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,58 @@ namespace ChapeauUI
         public ManagerViewMenu()
         {
             InitializeComponent();
+
+            DisplayMenu();
+            
+        }
+
+        private void DisplayMenu()
+        {
+            try
+            {
+                MenuItemService menuService = new MenuItemService();
+                List<MenuItem> menuList = menuService.GetMenuItems();
+
+                listView_Menu_Management.Items.Clear();
+                string foorOrDrink = "";
+
+                foreach (MenuItem item in menuList)
+                {
+
+                    if(item.isFood)
+                    {
+                        foorOrDrink = "Food";
+                    }
+                    else
+                    {
+                        foorOrDrink = "Drink";
+                    }
+
+                    string[] output = { item.MenuItemID.ToString(), item.MenuItemName, item.MenuItemType.ToString(), item.MenuItemCategory.ToString(),
+                                        foorOrDrink, item.MenuItemPrice.ToString("€0.00"), item.MenuItemStock.ToString(), item.AveragePreparationTime.ToString() };
+                    ListViewItem list = new ListViewItem(output);
+                    listView_Menu_Management.Items.Add(list);
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Something went wrong while loading the menu: " + e.Message);
+            }
+        }
+
+        private void ManagerViewMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Add_Menu_Item_Click(object sender, EventArgs e)
+        {
+            AddMenuItem addItem = new AddMenuItem();
+            addItem.Show();
+            this.Hide();
         }
     }
+
+
 }
